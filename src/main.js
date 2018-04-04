dff4b02526999fd3884808ea74655b63e8917cb0
gongshichun <gongshichun@outlook.com>
2018-04-04 18:57:56 +0800

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'jquery'
+
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
