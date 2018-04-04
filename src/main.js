a82e7a074b176aef60ad569e02b29e6d1fd077a9
gongshichun <gongshichun@outlook.com>
2018-04-04 18:42:07 +0800

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
